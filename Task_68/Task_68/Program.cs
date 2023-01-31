78b98c7cb8078b724904dea89e7ab62c558ad353
STGorbunovDA <bous07@mail.ru>
2023-01-31 08:42:28 +0800

    Write a program for calculating the Ackerman function using recursion. Two non-negative numbers m and n are given.

---
+++ Task_68/Task_68/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Task_68
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+            //Даны два неотрицательных числа m и n.
+            //m = 2, n = 3->A(m, n) = 9
+            //m = 3, n = 2->A(m, n) = 29
+
+            while (true)
+            {
+                int m = IntRead("Введите M:");
+                int n = IntRead("Введите N:");
+
+                if (m < 0 && n < 0)
+                {
+                    Console.WriteLine("Введи целое натуральное число");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            int Akkerman(int m, int n)
+            {
+                if (m == 0) return n + 1;
+                if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+                else return Akkerman(m - 1, Akkerman(m, n - 1));
+            }
+
+
+            int IntRead(string stringNumber)
+            {
+                Console.WriteLine(stringNumber);
+                stringNumber = Console.ReadLine().Trim();
+                while (!int.TryParse(stringNumber, out int number))
+                {
+                    Console.WriteLine("Ошибка! Введите целое число!");
+                    stringNumber = Console.ReadLine().Trim();
+                }
+                Console.Clear();
+                return Convert.ToInt32(stringNumber);
+            }
+        }
+    }
+}
