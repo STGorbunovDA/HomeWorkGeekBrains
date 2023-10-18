22521c100afc16e689e9cef839b4a2d45b01cfae
Дмитрий Горбунов <bous07@mail.ru>
2023-10-18 14:07:44 +0800

    Add Task_74

---
+++ Task_74/ArrayIComparer/ArrayIComparer/CustomIntComparer.cs
@@ -0,0 +1,23 @@
+﻿namespace ArrayIComparer
+{
+    public class CustomIntComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            //сортировка всех элементов от большого к малому
+            //if ((x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1))
+            //    return y.CompareTo(x);
+
+            //if (x % 2 == 1) return 1;
+            //else return -1;
+
+            //сортировка чётных влево от малого к большому и нечётных вправа от малого к большому
+            if ((x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1)) return x.CompareTo(y);
+            else
+            {
+                if (x % 2 == 1) return 1;
+                else return -1;
+            }
+        }
+    }
+}
---
+++ Task_74/ArrayIComparer/ArrayIComparer/Program.cs
@@ -0,0 +1,9 @@
+﻿using ArrayIComparer;
+
+IComparer<int> comparer = new CustomIntComparer();
+
+int[] arr = new int[] { 1, 4, 2, 3 };
+
+Array.Sort(arr, comparer);
+foreach (int i in arr)
+    Console.Write(i + " ");
