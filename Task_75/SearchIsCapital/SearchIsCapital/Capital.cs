4e99d24f0dd663cc92336515318f773f5e2ec8d1
Дмитрий Горбунов <bous07@mail.ru>
2023-10-18 14:44:48 +0800

    Add Task_75

---
+++ Task_75/SearchIsCapital/SearchIsCapital/Capital.cs
@@ -0,0 +1,17 @@
+﻿namespace SearchIsCapital
+{
+    public static class Capital
+    {
+        public static bool IsCapital(string s)
+        {
+            foreach (char c in s)
+                if (!Char.IsUpper(c)) return false;
+            return true;
+        }
+
+        public static bool IsEvenRemove(int i)
+        {
+            return i % 2 == 0;
+        }
+    }
+}
---
+++ Task_75/SearchIsCapital/SearchIsCapital/Program.cs
@@ -0,0 +1,31 @@
+﻿using SearchIsCapital;
+using System.Threading.Channels;
+
+var sx = new List<string> { "AaAaA", "AAAAA", "aaa", "BBBBB" };
+var ints = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var x = sx.FindIndex(Capital.IsCapital);
+
+if (x>=0)
+{
+    Console.WriteLine(x);
+    x++;
+}
+
+while (x>=0 && x< sx.Count)
+{
+    x = sx.FindIndex(x, Capital.IsCapital);
+
+    if (x >= 0)
+    {
+        Console.WriteLine(x);
+        x++;
+    }
+}
+
+var res = ints.RemoveAll(x => x % 2 == 0);
+
+Console.WriteLine($"Чётных чисел: {res}");
+
+ints.ForEach(Console.WriteLine);
+
+Console.ReadLine();
