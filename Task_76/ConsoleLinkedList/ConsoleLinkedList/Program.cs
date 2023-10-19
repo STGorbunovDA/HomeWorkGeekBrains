2c0e13ff556054017978e788ced684818afaf59d
Дмитрий Горбунов <bous07@mail.ru>
2023-10-19 22:44:23 +0800

    add task_76

---
+++ Task_76/ConsoleLinkedList/ConsoleLinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿var ints = new LinkedList<int>(new int[] {  2, 3, 4, 5, 6, 7 });
+
+ints.AddBefore(ints.First, 1); 
+ints.AddFirst(0);
+ints.AddLast(8);
+//ints.Clear();
+ints.Remove(ints.First);
+ints.RemoveFirst();
+ints.RemoveLast();
+
+var node = ints.First; // ints.Last;
+while (node != null)
+{
+    Console.Write(node.Value + " ");
+    node = node.Next; // node.Previous;
+}
+Console.ReadLine();
