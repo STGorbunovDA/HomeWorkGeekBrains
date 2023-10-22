506f201dcc8848f67c99b392d81c93a273c4b669
Дмитрий Горбунов <bous07@mail.ru>
2023-10-22 12:16:08 +0800

    Add Task_80

---
+++ Task_80/DicGetHasCode/DicGetHasCode/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace DicGetHasCode;
+
+class Program
+{
+    static int[] buckets = new int[10];
+    static DictionaryEntry[] entries = new DictionaryEntry[10];
+    static int c = 0;
+
+    static void Add(object key, object value)
+    {
+        int bucketNum = key.GetHashCode() & 0x7fffffff % buckets.Length;
+        buckets[bucketNum] = c;
+        entries[c].Value = value;
+        c++;
+    }
+
+    static object Get(object key)
+    {
+        int bucketNum = key.GetHashCode() & 0x7fffffff % buckets.Length;
+
+        return entries[buckets[bucketNum]].Value;
+    }
+
+    static void Main(string[] args)
+    {
+        Add(5, "Element5");
+        Add(6, "Element6");
+
+        Console.WriteLine(Get(5));
+        Console.WriteLine(Get(6));
+    }
+}
+
