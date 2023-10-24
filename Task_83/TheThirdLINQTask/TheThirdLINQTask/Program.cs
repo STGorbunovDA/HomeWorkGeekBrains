eb26176c2e455501276addf7ea640ed991a37dad
Дмитрий Горбунов <bous07@mail.ru>
2023-10-24 20:11:29 +0800

    Add Task_83

---
+++ Task_83/TheThirdLINQTask/TheThirdLINQTask/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+     * В этой задаче у вас есть список строк, и ваша задача – отсортировать этот список 
+     * в порядке возрастания длины строк, используя лямбда-выражение. 
+     * Ниже приведены начальные данные и возможное решение:
+     
+        * 
+*/
+
+
+namespace TheThirdLINQTask;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Repository repository = new Repository();
+
+        var sortedList = repository.strings.OrderBy(s => s.Length).ToList();
+
+        sortedList.ForEach(Console.WriteLine);
+    }
+}
---
+++ Task_83/TheThirdLINQTask/TheThirdLINQTask/Repository.cs
@@ -0,0 +1,15 @@
+﻿namespace TheThirdLINQTask
+{
+    public class Repository
+    {
+        public List<string> strings = new List<string>
+        {
+            "Apple",
+            "Banana",
+            "Cherry",
+            "Date",
+            "Fig",
+            "Grapes"
+        };
+    }
+}
