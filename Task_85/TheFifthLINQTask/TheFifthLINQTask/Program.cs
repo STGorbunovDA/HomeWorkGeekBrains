f7339f2abcc1584ddc72ce0355f581d36701e304
Дмитрий Горбунов <bous07@mail.ru>
2023-10-24 20:42:17 +0800

    Add Task_85

---
+++ Task_85/TheFifthLINQTask/TheFifthLINQTask/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+     * Поиск общих элементов в двух коллекциях
+*/
+
+namespace TheFifthLINQTask;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Repository repository = new Repository();
+
+        var result = repository.hashSet.Intersect(repository.list);
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ Task_85/TheFifthLINQTask/TheFifthLINQTask/Repository.cs
@@ -0,0 +1,8 @@
+﻿namespace TheFifthLINQTask
+{
+    public class Repository
+    {
+        public HashSet<int> hashSet = new HashSet<int> { 1, 2, 3, 4, 5 };
+        public List<int> list = new List<int> { 3, 4, 5, 6, 7 };
+    }
+}
