1dc2304d666ff563aae792ba5312dea61a53c9b2
Дмитрий Горбунов <bous07@mail.ru>
2023-10-27 21:37:46 +0800

    Add Task_88

---
+++ Task_88/ConvertStrings/ConvertStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    * Создайте метод, который принимает список строк, функцию (делегат Func) для преобразования строк в числа 
+      и действие (делегат Action) для выполнения какого-либо действия с числами.
+*/
+
+namespace ConvertStrings;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>() { "1", "2", "3", "4" };
+
+        Parse(list, int.Parse, (x) => Console.WriteLine(x));
+    }
+
+    static void Parse(List<string> listStr, Func<string, int> func, Action<int> action)
+    {
+        foreach (string item in listStr)
+        {
+            int res = func(item);
+            action(res);
+        }
+    }
+}
+
